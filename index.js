1b79ee87602e6db417cf429fd009b9f3aec65eb9
maquino96 <matthewaquino96@gmail.com>
2021-02-21 17:56:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names){
+    return names.map( el => `Thank you, ${el}, for the wonderful surprise gift!` )
+}
+
+function countDown(num) {
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
